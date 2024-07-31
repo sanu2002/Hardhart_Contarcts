045428da1a14e409c9d0f5a8946674b92d10b1fc
sanu <sanujitmajhi473@gmail.com>
2024-07-31 12:59:14 +0530

    hardhart_commits

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.24",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
+};
